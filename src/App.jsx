b99517e0097282681ba5535145031b08d153fae8
devjose <jose@gmail.com>
2023-07-09 07:49:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <main className="bg-red-600 w-screen h-screen">
+        <div className="w-3/12 h-screen bg-blue-50">
+          <div className="flex w-full h-1/4 bg-fuchsia-300">
+
+          </div>
+        </div>
+        <div className="w-9/12 bg-blue-500" ></div>
+      </main>
+    </>
+  );
+};
