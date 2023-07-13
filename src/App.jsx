21ed2abc8bc5cc0d7d28bd9ed318f9e111e37c5b
devjose <jose@gmail.com>
2023-07-13 08:51:14 +0200

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Profle } from "./components/Profle";
 import { Nav } from "./components/Nav";
-import { Routes, Route, BrowserRouter as Router, Navigate, HashRouter } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+  HashRouter,
+} from "react-router-dom";
 import { Home, Certificates, About, Contact, Projects } from "./pages";
 export const App = () => {
   return (
@@ -18,17 +24,17 @@ export const App = () => {
           </div>
           {/* Informacion de opcion */}
           <div className="w-5/6 overflow-auto ">
-            <Routes>
-              <HashRouter>
-              <Route path="/app" element={<App />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/certificates" element={<Certificates />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/*" element={<Navigate to={'/home'}/>} />
-              </HashRouter>
-            </Routes>
+            <HashRouter>
+              <Routes>
+                <Route path="/app" element={<App />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/certificates" element={<Certificates />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/*" element={<Navigate to={"/home"} />} />
+              </Routes>
+            </HashRouter>
           </div>
         </main>
       </Router>
